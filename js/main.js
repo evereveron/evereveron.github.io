0a7e4561c1f25012a861f8bae39aa83a97b107e9
Jasmine Feng <Jasmine Feng>
2017-04-16 23:37:38 -0400

    contact info

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  //click handlers for categories
+  var about_me = document.getElementById('about-me'),
+   coding = document.getElementById('coding'),
+   creating = document.getElementById('creating');
+
+   console.log('hi')
+});
