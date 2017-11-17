1e580b8518581bfae4c68cb5bcc12296ba668d03
Jasmine Feng <Jasmine Feng>
2017-11-16 22:41:57 -0500

    switchable sections

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
+let categories = [
+  'about-me',
+  'coding',
+  'creating'
+];
+
 document.addEventListener("DOMContentLoaded", function(event) {
   //click handlers for categories
-  var about_me = document.getElementById('about-me'),
-   coding = document.getElementById('coding'),
-   creating = document.getElementById('creating');
+  categories.forEach((category) => {
+    document.getElementById(category).addEventListener('click', (e) => {
+      clearActiveCategory();
+      setActiveCategory(e.target.id);
+    });
+  });
 
-   console.log('hi')
 });
+
+// === helper functions ===
+function setActiveCategory (section) {
+  document.getElementById(section).classList.add('purple');
+  document.getElementById(section + '-section').classList.add('active');
+}
+
+function clearActiveCategory () {
+  categories.forEach((category) => {
+    document.getElementById(category).classList.remove('purple');
+    document.getElementById(category + '-section').classList.remove('active');
+  })
+}
